3748d4f8f5dfa34bd136d208aae04884b84bab11
AlvarDev <deivid07.21@gmail.com>
2016-06-17 19:10:52 -0300

    sp

--- sql/database.sql
+++ sql/database.sql
@@ -17,8 +17,9 @@ CREATE TABLE amistades(
 	PRIMARY KEY (codusu, codami),
 	FOREIGN KEY (estado) REFERENCES usuario(codusu)
 );
-
--- REGISTRAR NUEVO USUARIO
+---------------------------------------------------------------------------------------------------------------------------------------
+-- (0) REGISTRAR NUEVO USUARIO
+---------------------------------------------------------------------------------------------------------------------------------------
 DELIMITER //
 CREATE PROCEDURE sp_register_new_user(
 	IN emailIn varchar(21),
@@ -36,7 +37,9 @@ DELIMITER ;
 CALL sp_register_new_user('alvardev@google.com','brasil21','AlvarDev', 'Android Dev', 'https://scontent.fcwb1-1.fna.fbcdn.net/v/t1.0-9/12472824_761620513937453_3396642389945947222_n.jpg?oh=50ade16485988b84cefd506fb36a7c5e&oe=57D1538E');
 CALL sp_register_new_user('pao@google.com', 'peru21', 'Pao', 'Software Dev', 'https://scontent.fcwb1-1.fna.fbcdn.net/t31.0-8/q81/s960x960/13041292_1068079236588381_4231572010446298894_o.jpg');
 
--- LOGIN
+---------------------------------------------------------------------------------------------------------------------------------------
+-- (1) LOGIN
+---------------------------------------------------------------------------------------------------------------------------------------
 DELIMITER //
 CREATE PROCEDURE sp_login(
 	IN emailIn varchar(21),
@@ -49,21 +52,47 @@ DELIMITER ;
 
 CALL sp_login('alvardev@google.com','brasil21');
 
--- OBTENER PERFIL
+---------------------------------------------------------------------------------------------------------------------------------------
+-- (2) OBTENER PERFIL
+---------------------------------------------------------------------------------------------------------------------------------------
 DELIMITER //
 CREATE PROCEDURE sp_get_profile(
-	IN codusuIn int)
+	IN codusuIn int,
+  IN codamiIn int,
+  IN type int)
 BEGIN
+
+  IF type == 0 -- OWENER Profile
   SELECT nomusu, profesion, foto from usuario where codusu=codusuIn;
+  ELSE
+  SELECT nomusu, profesion, foto 
+  (select estado from amistades where (codusu=codusuIn and codami=codamiIn) or (codusu=codami and codami=codamiIn)) as estado
+  from usuario where codusu=codusuIn;
+  END IF
 END //
 DELIMITER ;
 
 CALL sp_get_profile(1);
 
--- BUSCAR USUARIOS
+---------------------------------------------------------------------------------------------------------------------------------------
+-- (3) LISTAR AMIGOS
+---------------------------------------------------------------------------------------------------------------------------------------
+DELIMITER //
+CREATE PROCEDURE sp_get_list_friends(
+	IN codusuIn int)
+BEGIN
+  SELECT codusu, nomusu, foto from usuario where codusu=codusuIn;
+END //
+DELIMITER ;
+
+CALL sp_get_list_friends(1);
+
+---------------------------------------------------------------------------------------------------------------------------------------
+-- (4) BUSCAR USUARIOS
+---------------------------------------------------------------------------------------------------------------------------------------
 DELIMITER //
 CREATE PROCEDURE sp_search_users(
-	IN nomusuIn varchar(21))
+  IN nomusuIn varchar(21))
 BEGIN
   SELECT codusu, nomusu, foto from usuario where nomusu like '%'+nomusuIn+'%';
 END //
@@ -71,20 +100,47 @@ DELIMITER ;
 
 CALL sp_search_users('Pao');
 
--- ADMINISTRAR AMISTAD
+---------------------------------------------------------------------------------------------------------------------------------------
+-- (5) ADMINISTRAR AMISTAD
+---------------------------------------------------------------------------------------------------------------------------------------
 DELIMITER //
 CREATE PROCEDURE sp_manage_friendship(
-	IN codusuIn int,
-    IN codamiIn int,
-    IN estadoIn int)
+  IN codusuIn int,
+  IN codamiIn int,
+  IN estadoIn int)
+
 BEGIN
+  DECLARE exist1 int;
+  DECLARE exist2 int;
+  SELECT estado INTO exist1
+  FROM amistades
+  where codusu=codusuIn and codami=codamiIn;
+
+  SELECT estado INTO exist2
+  FROM amistades
+  where codusu=codamiIn and codami=codamiIn;
+
+  IF exist1 ==  1
+  UPDATE amistades
+  SET estado = estadoIn
+  where codusu = codusuIn and codami=codamiIn;
+  ELSE IF exist2 ==  1
+  UPDATE amistades
+  SET estado = estadoIn
+  where codusu = codamiIn and codami=codusuIn;
+  ELSE
   INSERT INTO amistades (codusu, codami, estado)
   values(codusuIn, codamiIn, estadoIn);
+  END IF
+
 END //
 DELIMITER ;
 
 CALL sp_manage_friendship(1, 2 , 3);
 CALL sp_manage_friendship(1, 2 , 2);
+
+
+
 
 
 
